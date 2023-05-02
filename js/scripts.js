8723d8f15fe04c97de5ed693940d00700228f853
Brian Scherner <brianscherner@gmail.com>
2023-05-01 22:18:12 -0700

    Add functions converting liters to quarts and liters to gallons. Add functions converting milliliters to cups and milliliters to ounces. Declare variables for numbers 3, 4, 5, and 6. Declare variables for unit. Declare variables for a, b, c, d, e, and f, with each variable corresponding to the sum of its respective function. Declare variables for miString, ftString, qtString, galString, cupString and ozString. Update window alert to include qtString, galString, cupString and ozString concatenated into previous window alert.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,46 @@
-function multiply(km, number1) {
-	return km * number1;
+function multiply(unit, number1) {
+	return unit * number1;
 }
 
-function multiply(km, number2) {
-	return km * number2;
+function multiply(unit, number2) {
+	return unit * number2;
 }
 
-const km = parseInt(prompt("Enter a unit:"))
+function multiply(unit, number3) {
+	return unit * number3;
+}
+
+function multiply(unit, number4) {
+	return unit * number4;
+}
+
+function multiply(unit, number5) {
+	return unit * number5;
+}
+
+function multiply(unit, number6) {
+	return unit * number6;
+}
+
+const unit = parseInt(prompt("Enter a unit:"))
 const number1 = 0.62;
 const number2 = 3280.8;
-const a = multiply(km, number1);
-const b = multiply(km, number2);
+const number3 = 1.057;
+const number4 = 0.264;
+const number5 = 0.0042
+const number6 = 0.0338;
+const a = multiply(unit, number1);
+const b = multiply(unit, number2);
+const c = multiply(unit, number3);
+const d = multiply(unit, number4);
+const e = multiply(unit, number5);
+const f = multiply(unit, number6);
 
-let miString = km + " kilometers" + " = " + a + " miles";
-let ftString = km + " kilometers" + " = " + b + " feet";
+let miString = unit + " kilometers" + " = " + a + " miles";
+let ftString = unit + " kilometers" + " = " + b + " feet";
+let qtString = unit + " liters" + " = " + c + " quarts";
+let galString = unit + " liters" + " = " + d + " gallons";
+let cupString = unit + " milliliters" + " = " + e + " cups";
+let ozString = unit + " milliliters" + " = " + f + " ounces";
 
-window.alert(miString + ". " + ftString + ". ");
+window.alert(miString + ". " + ftString + ". " + qtString + ". " + galString + ". " + cupString + ". " + ozString + ". ");
